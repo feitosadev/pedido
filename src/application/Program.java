a900131329ab5a302805b09ee56c9d787db9b32d
José Carlos <jccfjr@gmail.com>
2025-08-16 15:32:06 -0300

    PRimeiro commit projeto pedido.

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Product produtc1 = new Product("Notebook", 3999.99);
+		
+		
+
+	}
+}
